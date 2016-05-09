7ef19d283dd974d8f766a29b937b6cb53a326faf
Gregory Beaver <greg@chiaraquartet.net>
2016-05-08 19:15:53 -0500

    100% coverage for MenuItem

--- imports/client/modules/navigation/components/MenuItem.test.jsx
+++ imports/client/modules/navigation/components/MenuItem.test.jsx
@@ -30,7 +30,7 @@ describe('core.components.MenuItem', () => {
     const onButtonClick = sinon.spy()
     const el = shallow(<MenuItem link={null} onClick={onButtonClick} />)
     el.find('li').simulate('click')
-    expect(onButtonClick.calledOnce).to.be.true
+    expect(onButtonClick.calledOnce).to.be.equal(true)
   })
 
   it('no link: should set active if specified', () => {
@@ -38,5 +38,18 @@ describe('core.components.MenuItem', () => {
     expect(el.find('a').node.props.children[1].type).to.equal('span')
     expect(el.find('a').node.props.children[1].props.className).to.equal('sr-only')
     expect(el.find('a').node.props.children[1].props.children).to.equal('(current)')
+  })
+
+  it('no link: clicking on the link should prevent default', () => {
+    const onButtonClick = sinon.spy()
+    const el = shallow(<MenuItem link={null} active />);
+    const a = {
+      preventDefault: () => {
+        onButtonClick()
+      }
+    }
+
+    el.find('a').simulate('click', a)
+    expect(onButtonClick.calledOnce).to.be.equal(true)
   })
 });
