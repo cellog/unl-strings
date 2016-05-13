1b20c8f0062070b0f3db97101e2cfd872fed58f3
Gregory Beaver <greg@chiaraquartet.net>
2016-05-13 01:08:13 -0500

    refactor TextAreaField to use FormElement

--- imports/client/modules/form/components/TextAreaField.jsx
+++ imports/client/modules/form/components/TextAreaField.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
+import FormElement from './FormElement.jsx'
 const TextAreaField = ({label = 'TextArea Field', onChange = () => null,
-    id, placeholder = '', value = '', help = '', rows = null, fieldclass = '',
-    labelclass = '', containerClass = ''}) => {
+    placeholder = '', value = '', rows = null, fieldclass = '',
+    ...props}) => {
   const c = fieldclass ? ` ${fieldclass}` : ''
   const myclass = () => `form-control${c}`
   return (
-    <div>
-      <label htmlFor={id} className={labelclass}>{label}</label>
-      <div className={containerClass}>
-        <textarea rows={rows ? rows : null } className={myclass()} id={id} placeholder={placeholder}
-                  onChange={(e) => onChange(e.target.value)} value={value}/>
-        {help ? <p className="help-block">{help}</p> : ''}
-      </div>
-    </div>
+    <FormElement label={label} {...props}>
+      <textarea rows={rows ? rows : null } className={myclass()} id={props.id} placeholder={placeholder}
+                onChange={(e) => onChange(e.target.value)} value={value}/>
+    </FormElement>
   )
 }
 
--- imports/client/modules/form/components/TextAreaField.test.jsx
+++ imports/client/modules/form/components/TextAreaField.test.jsx
@@ -1,25 +1,25 @@
 const {describe, it} = global
 import {expect} from 'chai'
-import {shallow} from 'enzyme'
+import {shallow, mount} from 'enzyme'
 import React from 'react'
 import TextAreaField from './TextAreaField.jsx'
 
 describe('form.components.TextAreaField', () => {
   it('should work', () => {
-    const el = shallow(<TextAreaField id="hi"/>)
+    const el = mount(<TextAreaField id="hi"/>)
     expect(el.find('label')).to.have.length(1)
-    expect(el.find('label').node.props.htmlFor).to.equal('hi')
+    expect(el.find('label').props().htmlFor).to.equal('hi')
 
     expect(el.find('textarea')).to.have.length(1)
-    expect(el.find('textarea').node.props.className).to.equal('form-control', 'class')
-    expect(el.find('textarea').node.props.id).to.equal('hi', 'id')
-    expect(el.find('textarea').node.props.placeholder).to.equal('', 'placeholder')
+    expect(el.find('textarea').props().className).to.equal('form-control', 'class')
+    expect(el.find('textarea').props().id).to.equal('hi', 'id')
+    expect(el.find('textarea').props().placeholder).to.equal('', 'placeholder')
   })
 
   it('should have id set', () => {
-    const el = shallow(<TextAreaField id="myid" />)
-    expect(el.find('label').node.props.htmlFor).to.equal('myid')
-    expect(el.find('textarea').node.props.id).to.equal('myid')
+    const el = mount(<TextAreaField id="myid" />)
+    expect(el.find('label').props().htmlFor).to.equal('myid')
+    expect(el.find('textarea').props().id).to.equal('myid')
   })
   
   it('should update value with onChange', () => {
@@ -37,11 +37,11 @@ describe('form.components.TextAreaField', () => {
   })
   
   it('should display help', () => {
-    const el = shallow(<TextAreaField id="hi" help="Here is the help you need"/>)
+    const el = mount(<TextAreaField id="hi" help="Here is the help you need"/>)
 
     expect(el.find('p').nodes).to.have.length(1)
-    expect(el.find('p').node.props.className).to.equal('help-block')
-    expect(el.find('p').node.props.children).to.equal('Here is the help you need')
+    expect(el.find('p').props().className).to.equal('help-block')
+    expect(el.find('p').props().children).to.equal('Here is the help you need')
   })
 
   it('should set rows', () => {
@@ -62,14 +62,14 @@ describe('form.components.TextAreaField', () => {
   })
 
   it('should have label set', () => {
-    const el = shallow(<TextAreaField id="hi" placeholder="placeholder"/>)
+    const el = mount(<TextAreaField id="hi" placeholder="placeholder"/>)
 
-    expect(el.find('label').node.props.children).to.equal('TextArea Field')
+    expect(el.find('label').props().children).to.equal('TextArea Field')
   })
 
   it('should have new label set', () => {
-    const el = shallow(<TextAreaField id="hi" label="placeholder"/>)
+    const el = mount(<TextAreaField id="hi" label="placeholder"/>)
 
-    expect(el.find('label').node.props.children).to.equal('placeholder')
+    expect(el.find('label').props().children).to.equal('placeholder')
   })
 })
