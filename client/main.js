49d229fad2d0b5a30482b6d2beacd53cf7a7907f
Gregory Beaver <greg@chiaraquartet.net>
2016-05-13 16:54:10 -0500

    remove unused code/packages

--- client/main.js
+++ client/main.js
@@ -18,17 +18,3 @@ Meteor.startup(() => {
     document.getElementById('root')
   )
 })
-
-//import { createApp } from 'mantra-core';
-//import initContext from '../imports/client/configs/context';
-
-// modules
-//import coreModule from '../imports/client/modules/core';
-
-// init context
-//const context = initContext();
-
-// create app
-//const app = createApp(context);
-//app.loadModule(coreModule);
-//app.init();
